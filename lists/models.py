27aaf4206c454e01de0a8386020fa90fbe5b590e
Demo User <demouser@n245-52.mtholyoke.edu>
2016-01-06 10:45:15 -0500

    Added a first model for Item.

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# We now have an item available, models.model gives us a save attribute
+# Python is the programming language that describes the syntax of Django,
+# Django is a library that uses Python syntax
+class Item(models.Model):
+    # pass
+# from an obj oriented perspective
+# We have an item that is text, that is a string of data
+    text = models.TextField(default='')
+
+# run make migrations every time you change the database structure
+#programs don't like making decisions, they like programmers to make decisions
--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,10 @@
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
-from lists.views import home_page
 from django.http import HttpRequest
+from lists.views import home_page
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
 
@@ -39,3 +41,24 @@ class HomePageTest(TestCase):
         'home.html', {'new_item_text': 'A new list item'})
 
         self.assertEqual(response.content.decode(), expected_html)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        # call constructor as function and returns instance of object, don't need new instance
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        # retrieve saved items
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        #check that items line up
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
